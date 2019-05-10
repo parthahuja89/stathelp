d790b1d7b553e91d5e1a4f6c1854ccec6862ffc4
theoldclassified <parthahuj@gmail.com>
2019-05-10 17:22:49 -0400

    frontend design

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './App.css'
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Menu';
+import Icon from '@material-ui/core/Icon';
 const styles = {
-  card: {
-    minWidth: 275,
+  card: { 
+    minWidth: '70%',
+    minHeight: '70%',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'absolute'
   },
   bullet: {
     display: 'inline-block',
@@ -26,34 +33,37 @@ const styles = {
 
 function App(props) {
   const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div>
+      {/*
+      TODO: change logo font+color
+      */}
+      <Grid
+        container
+        direction="row"
+        justify="flex-end"
+        alignItems="flex-start"
+      >
+      <Grid Item xs={11}> 
+      <div id='logo'>
+          StatHelp 
+      </div>
+      </Grid>
+
+      <Grid align ='right' item xs={1}>
+          <Fab color="white" aria-label="Add">
+          <AddIcon color='#000000'/>
+          </Fab>
+      </Grid>
+      
+      </Grid>
+      <Card className={classes.card}>
+        <CardContent>
+          this is the stuff inside the card
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
