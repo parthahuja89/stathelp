44e969c92f36d39f50d7bd91b601e9f96dad7632
Parth Ahuja <parthahuj@gmail.com>
2019-05-31 13:30:04 -0400

    dist page updates

--- react-app/src/Utils.js
+++ react-app/src/Utils.js
@@ -9,11 +9,21 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import triangle_url from '../assets/utils/triangle_boy.svg'
 //matrial styles 
 const styles = {
     card: { 
         width: '90%',
-        maxWidth: '800px',
+        maxWidth: '1000px',
         minHeight: '80%',
         left: '50%',
         top: '50%',
@@ -31,6 +41,9 @@ const styles = {
         marginLeft: -12,
         marginRight: 20,
     },
+    textfield:{
+        width: 20, 
+    },
 };
 
 
@@ -43,6 +56,7 @@ class Utils extends React.Component {
         const { classes } = this.props; //props for material ui
     return(
         <div class='utils'>
+        <div class='title'>Distributions and Utilities</div>
         <Card width='50%' className={classes.card}>
               <CardContent>
               <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none'}}>
@@ -55,14 +69,43 @@ class Utils extends React.Component {
             textColor="primary"
             centered
             >
-            <Tab label="Item One" />
-            <Tab label="Item Two" />
-            <Tab label="Item Three" />
+            <Tab label="Central Tendencies" />
+            <Tab label="Center Distributions" />
+            <Tab label="Probability Distributions" />
             </Tabs>
          </Grid>
         </Toolbar>
-        </AppBar> 
-              </CardContent>
+        </AppBar>
+        {/*Card Content depeneds on tab selections*/}
+                <TextField
+                id="standard-full-width"
+                label="Input Data"
+                style={{ margin: 8 }}
+                placeholder="Example: 12, 33, 44"
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                shrink: true,
+                }}
+            />
+            Choose Operation: 
+            {/*Menu for operation selection*/}
+                <FormControl className={classes.formControl}>
+                    <Select
+                    displayEmpty
+                    name="age"
+                    className={classes.selectEmpty}
+                    >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                    <FormHelperText>Without label</FormHelperText>
+            </FormControl>
+        </CardContent>
           </Card>
         </div>
     );    
