b0ece7627f5db1a8ec74fb0545095db1ead128c7
theoldclassified <parthahuj@gmail.com>
2019-07-09 18:46:50 -0400

    resolving merge with linux

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Dist from './Distributions';
 import Tendency from './Tendency';
+import Graphing from './Graphing';
 import {BrowserRouter} from 'react-router-dom'
 import {Route} from 'react-router-dom'
 
@@ -13,6 +14,7 @@ ReactDOM.render(
   <Route exact path='/' component={App}/>
   <Route exact path='/utils' component={Tendency}/>
   <Route exact path='/Dist' component={Dist}/>
+  <Route exact path= '/Graphing' component = {Graphing}/>
   </BrowserRouter>,
   document.getElementById('root')
 );
