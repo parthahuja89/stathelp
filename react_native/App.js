e5041afe4f368e41f17e1328562cb238a7b26f86
theoldclassified <parthahuj@gmail.com>
2019-07-11 16:48:34 -0400

    added flexbox to front page --reactnative

--- react_native/App.js
+++ react_native/App.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import * as React from 'react';
-import { Alert, ScrollView, StyleSheet , Image} from 'react-native';
+import { Alert, ScrollView, StyleSheet , Image, Text, View} from 'react-native';
 import {
   Title,
   Caption,
@@ -9,34 +9,58 @@ import {
   Card,
   Button,
   withTheme,
+  Dialog, Portal,
 } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import { IconButton, Colors } from 'react-native-paper';
 
 
 class App extends React.Component {
   static title = 'Card';
+  state = {
+    dialog: false,
+    dialog_text: '',
+
+  };
+  _showDialog = (text) => this.setState({ 
+    dialog: true,
+    dialog_text: text
+   });
+
+  _hideDialog = () => this.setState({ dialog: false });
 
   render() {
     
     return (
+      <PaperProvider>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.content}
       >
+      {/** Row Flexbox  */}
+      <View style= {styles.inputWrap}>
+
+        <Image source={require('./assets/main_page_svgs/logo.png')} style ={styles.logo_image}/>
+        
+        <Text style ={styles.Logo}>
+        StatHelp
+        </Text>
+
         <IconButton
           icon="menu"
           color={Colors.black}
           size={30}
           onPress={() => console.log('Pressed')}
         />
-        
+      </View>
+
         <Card style={styles.card}>
           <Card.Content style={styles.aligments} >
               <Title style={styles.aligments}>Utilities and Centeral Tendencies</Title>
               <Image source={require('./assets/main_page_svgs/dist.png')} style = {styles.dist}/>
               <Card.Actions style={styles.aligments} >
-                <Button onPress={() => {}}> More Info </Button>
+                <Button onPress={() => this._showDialog('Includes Mean, Median, Mode, Variance, Standard Deviation, and Standard Error.')}> More Info </Button>
                 <Button onPress={() => {}}> Open </Button>
               </Card.Actions>
           </Card.Content>
@@ -47,7 +71,7 @@ class App extends React.Component {
               <Title style={styles.aligments}>Graphing Tools</Title>
               <Image source={require('./assets/main_page_svgs/graph.png')} style = {styles.graphing}/>
               <Card.Actions style={styles.aligments} >
-                <Button onPress={() => {}}> More Info </Button>
+                <Button onPress={() => this._showDialog('Includes graphing tools like bar charts, box plots, histogram, and pie charts.')}> More Info </Button>
                 <Button onPress={() => {}}> Open </Button>
               </Card.Actions>
           </Card.Content>
@@ -58,19 +82,35 @@ class App extends React.Component {
               <Title style={styles.aligments}>Probability Distributions</Title>
               <Image source={require('./assets/main_page_svgs/probability.png')} style = {styles.prob}/>
               <Card.Actions style={styles.aligments} >
-                <Button onPress={() => {}}> More Info </Button>
+                <Button onPress={() => this._showDialog('Includes probability distributions like bionomial, normal, poisson etc.')}> More Info </Button>
                 <Button onPress={() => {}}> Open </Button>
               </Card.Actions>
           </Card.Content>
         </Card>
 
+        {/** More Information Dialog */}
+        <Portal>
+          <Dialog
+             visible={this.state.dialog}
+             onDismiss={this._hideDialog}>
+            <Dialog.Title>More Information</Dialog.Title>
+            <Dialog.Content>
+              <Paragraph>{this.state.dialog_text}</Paragraph>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={this._hideDialog}>Okay</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
 
       </ScrollView>
+      </PaperProvider>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  //main container flexbox 
   container: {
     flex: 1,
   },
@@ -78,7 +118,22 @@ const styles = StyleSheet.create({
     padding: 2,
     justifyContent: 'center'
   },
-
+  //Row flex box for logo and iconbutton
+  inputWrap:{
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    padding: 8,
+  },
+  Logo:{
+    fontSize: 18,
+    top: 9,
+    left: -110,
+  },
+  logo_image:{
+    height: 30,
+    width: 35,
+  },
   dist:{
     height: 183,
     width: 241,
@@ -92,7 +147,7 @@ const styles = StyleSheet.create({
     width: 212,
   },
   card: {
-    margin: 25,
+    margin: 23,
     alignItems: 'center',
     textAlign: 'center'
   },
