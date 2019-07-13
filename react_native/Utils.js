4a0e612741b03e4d489c95cb9bc9cddefb2dbd1c
theoldclassified <parthahuj@gmail.com>
2019-07-13 14:21:49 -0400

    Added axios promising on utils page --react_native

--- react_native/Utils.js
+++ react_native/Utils.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import {View,Text, StyleSheet, ImageBackground} from 'react-native';
-import { Dimensions } from 'react-native';
-import { TextInput, List, Menu, Divider, Provider} from 'react-native-paper';
+import {View,Text, StyleSheet, ImageBackground, Picker} from 'react-native';
+import { Dimensions, Clipboard} from 'react-native';
+import { TextInput, List, Menu, Divider, Provider, Snackbar} from 'react-native-paper';
 
 import {
     Title,
@@ -11,24 +11,84 @@ import {
     Button,
     withTheme,
     Dialog, Portal,
+    IconButton,
+    DataTable  
   } from 'react-native-paper';
 import {ScrollView } from 'react-native-gesture-handler';
+
+import axios from 'axios';
 
 const win = Dimensions.get('window')
 
 class Utils extends React.Component {
+    constructor(){
+        super();
+        this.apiRequest = this.apiRequest.bind(this);
+        this.copytoclipboard = this.copytoclipboard.bind(this);
+    }
     state = {
         //textfield
         input_data: '',
-        //menu
-        visible: false,
+        //operation selection
+        operation: 'Arithmetic_Mean',
+        //warning snackbar
+        empty_data_warning: false,
+
+        //server output
+        answer: '',
+        showOutput: false,
+
+        //ouput copy to clipboard snackbar
+        copied: false,
     };
     
     _openMenu = () => this.setState({ visible: true });
 
     _closeMenu = () => this.setState({ visible: false });
   
+    /**
+     * Makes GET Request to Stathelp server 
+     * Json payload: {input_data: 'csv_values'}
+     */
+    apiRequest(){
+        console.log("Server request issued.")
 
+        //empty data safety check
+        if(this.state.input_data == ''){
+            //send warning 
+            this.setState({ empty_data_warning: true}) 
+            console.log("%cCan't perform requests on empty data, sending warning.", "color: red; font-size: 20px")
+        }else{
+
+        axios.get('http://192.168.1.6:5000/' + String(this.state.operation), {
+            //GET Request payload 
+            params: {
+                values: String(this.state.input_data)
+            }
+        })
+        .then(res =>{
+            //response 
+            var res_json = res.data
+            console.log("Server Response: " + JSON.stringify(res_json))
+            //Displaying output once state set
+            //Displaying output once state set
+            this.setState({
+                answer: res_json['Answer'],
+                showOutput: true
+            })
+        })
+        
+    }
+    }
+
+    /***
+     * Copies output to clipboard
+     */
+    copytoclipboard(){
+        console.log("Copying text to clipboard")
+        Clipboard.setString(this.state.answer)
+        this.setState({copied: true})
+    }
     render(){
         return(
             <Provider>
@@ -49,37 +109,76 @@ class Utils extends React.Component {
                     />
 
                     {/** Operation Selection Menu */}
-                    <Menu
-                        visible={this.state.visible}
-                        onDismiss={this._closeMenu}
-                        anchor={
-                        <Button onPress={this._openMenu}>Operation</Button>
-                        }
-                        style ={styles.menu}
 
-                    >
-                        <Menu.Item onPress={() => {}} title="Arithmetic Mean" />
-                        <Menu.Item onPress={() => {}} title="Geometric Mean" />
-                        <Menu.Item onPress={() => {}} title="Harmonic Mean" />
-                        <Menu.Item onPress={() => {}} title="Quadratic Mean" />
-                        <Menu.Item onPress={() => {}} title="Median" />
-                        <Menu.Item onPress={() => {}} title="Mode" />
-                        <Menu.Item onPress={() => {}} title="Range" />
-                        <Menu.Item onPress={() => {}} title="Variance(Population)" />
-                        <Menu.Item onPress={() => {}} title="Variance(Sample)" />
-                        <Menu.Item onPress={() => {}} title="Standard Deviation(Population)" />
-                        <Menu.Item onPress={() => {}} title="Standard Deviation(Sample)" />
-                        <Menu.Item onPress={() => {}} title="Standard Error of Mean" />
-                    </Menu>
-                     <Button  mode="contained" onPress={() => console.log('Pressed')} style={styles.button}>
+                    <Picker
+                        selectedValue={this.state.operation}
+                        style={styles.menu}
+                        onValueChange={(itemValue, itemIndex) =>
+                            this.setState({operation: itemValue, showOutput: false})
+                    }>
+                        <Picker.Item  label="Arithmetic Mean" value="Arithmetic_Mean" />
+                        <Picker.Item  label="Geometric Mean" value="Geometric_Mean" />
+                        <Picker.Item  label="Harmonic Mean" value="Harmonic_Mean" />
+                        <Picker.Item  label="Quadratic Mean" value="Quadratic_Mean" />
+                        <Picker.Item  label="Median" value="Median" />
+                        <Picker.Item  label="Mode" value="Mode" />
+                        <Picker.Item  label="Range" value="Range" />
+                        <Picker.Item  label="Variance(Population)" value="Variance" />
+                        <Picker.Item  label="Variance(Sample)" value="Variance_Sample" />
+                        <Picker.Item  label="Standard Deviation(Population)" value="Standard_Deviation_Population" />
+                        <Picker.Item  label="Standard Deviation(Sample)" value="Standard_Deviation_Sample" />
+                        <Picker.Item  label="Standard Error of Mean" value="Standard_Error" />
+
+                    </Picker>
+
+                     <Button  mode="contained" onPress={this.apiRequest} style={styles.button}>
                         Calculate
                     </Button>
 
-                </Card.Content>
+                    {/** Shows server response -- Hidden until res accepted*/}
+                    
+                    {this.state.showOutput &&
+                        <View style={styles.outputFlexbox}>
+ 
+                         <DataTable>
+                            <DataTable.Header>
+                            <DataTable.Title>Operation</DataTable.Title>
+                            <DataTable.Title numeric>Answer</DataTable.Title>
+                            </DataTable.Header>
+
+                            <DataTable.Row>
+                            <DataTable.Cell>{this.state.operation.replace(/_/g, ' ')}</DataTable.Cell>
+                            <DataTable.Cell numeric>{this.state.answer} 
+                            </DataTable.Cell>
+                            </DataTable.Row>
+                        </DataTable>
+                        <Button  onPress={this.copytoclipboard} style={styles.button}>
+                            Copy to clipboard
+                        </Button>  
+                        </View>
+                    }
+                </Card.Content>  
                 </Card>
-                
             </ScrollView>
             </ImageBackground>
+            {/** Incomplete data warning  */}
+            <Snackbar
+                    visible={this.state.empty_data_warning}
+                    onDismiss={() => this.setState({ empty_data_warning: false })}
+                    duration = {600}
+                    >
+                    Incomplete Data. 
+            </Snackbar>
+
+            {/** Copy to clipboard confirmation */}
+            <Snackbar
+                    visible={this.state.copied}
+                    onDismiss={() => this.setState({ copied: false })}
+                    duration = {600}
+                    >
+                    Copied to clipboard!
+            </Snackbar>
+
             </Provider>
            
         );
@@ -111,10 +210,28 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
     },
     button:{
-        top: 100,
+        top: 50,
     },
     menu:{
-        top: 50,
-    }
+        height:    100,                     
+        width: 300,                  
+        top: 50,                   
+    },
+    outputFlexbox:{
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+        top: 100,
+    },
+    output:{
+        alignItems: 'center',
+        textAlign: 'center',
+        fontSize: 17, 
+        top: 125, 
+    },
+    copy:{
+        top: 100,
+    },
 })
 export default Utils;
